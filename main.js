f3e089508dcf25b3ef2475166fe83fda354b1797
Matthew9029 <97785946+Matthew9029@users.noreply.github.com>
2023-06-16 17:19:33 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function draw(){
+    image(video, 0, 0, 300, 299);
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+    function modelLoaded(){
+        console.log('poseNet is initialized');
+    }
+
+function preload(){
+
+}
+
+function take_snapshot(){
+    save('myimage.png');
+}
+
+function gotPoses(results){
+    if(results.length >0){
+        console.log(results);
+        console.log("nose X = "+ results[0].pose.nose.x);
+        console.log("nose Y = "+ results[0].pose.nose.y);
+    }
+}
